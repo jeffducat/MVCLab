49c08580ac7c1fbbaae4a37bd9d8c32075f0ee37
jeff_ducat <jducat81@gmail.com>
2019-05-08 17:52:18 -0400

    This is MVC Lab-added validation

--- MVCCountriesLab/CountryController.cs
+++ MVCCountriesLab/CountryController.cs
@@ -16,15 +16,25 @@ namespace MVCCountriesLab
         public void WelcomeAction()
         {
             bool run = true;
+            Console.WriteLine("Hello! Welcome to the country app!");
             while (run)
             {
-                CountryDataBase countryDataBase = new CountryDataBase();
-                CountryListView countryListView = new CountryListView(countryDataBase.Countries);
-                Console.WriteLine("Hello, welcome to the country app.  Please select a country from the following list: ");
-                countryListView.Display();
+                Console.WriteLine("Would you like to select a country? Enter y to start or any other key twice to exit.");
+                string response = Console.ReadLine().ToLower();
+                if (response == "y")
+                {
+                    CountryDataBase countryDataBase = new CountryDataBase();
+                    CountryListView countryListView = new CountryListView(countryDataBase.Countries);
+                    Console.WriteLine("Please select a country from the following list: ");
+                    countryListView.Display();
 
-                int selection = int.Parse(Console.ReadLine());
-                CountryAction(countryDataBase.Countries[selection]);
+                    int selection = int.Parse(Console.ReadLine());
+                    CountryAction(countryDataBase.Countries[selection]);
+                }
+                else
+                {
+                    return;
+                }
             }
         }
     }
